97cffdfb656eeac4e9540640e3094384c8099414
Dana Onisa <dana.onisa@garmin.com>
2017-03-03 11:45:37 +0200

    initial commit

--- src/main/java/java8workshop/exercise/streams/ChapterFive.java
+++ src/main/java/java8workshop/exercise/streams/ChapterFive.java
@@ -3,7 +3,7 @@ package java8workshop.exercise.streams;
 import java.util.Arrays;
 import java.util.List;
 
-public class ChapterSix {
+public class ChapterFive {
 	
 	public static void main(String[] args) {
 		List<Integer> numbers1 = Arrays.asList(1, 2, 3);
